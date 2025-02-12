d8b7cb21981c92eac58a71e89dbf8ef040b1fa0e
Jacob Hepkema <jacobhepkema@gmail.com>
2025-02-12 09:24:18 +0000

    mod setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 
 
 def setup_package():
-    install_requires = ['numpy<2', 'pandas', 'pyBigWig', 'enformer-pytorch']
+    install_requires = ['numpy<2', 'pandas', 'pyBigWig', 'borzoi-pytorch']
     metadata = dict(
-        name='enformer_loader',
+        name='borzoi_loader',
         version='0.0.1',
-        description='enformer_loader',
-        url='https://github.com/jacobhepkema/enformer_loader',
+        description='borzoi_loader',
+        url='https://github.com/jacobhepkema/borzoi_loader',
         author='Jacob Hepkema',
         author_email='jacob.hepkema@sanger.ac.uk',
         license='MIT License',
